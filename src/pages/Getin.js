5d85f949e88f3c894ac9aff2377d7ea1d24ea7dc
AbdAska <Abdullahaskariaia@gmail.com>
2022-06-05 15:41:05 +0500

    first commit

--- src/pages/Getin.js
+++ src/pages/Getin.js
@@ -2,7 +2,37 @@ import image from '../Group5266.png';
 
 const Getin = () =>{
     return <div className="A29">
-     <img src={image} className='A30'></img>
+     <img src={image} style={{maxWidth:'43rem', flexShrink:'2'}}></img>
+     <div className='A30'>
+     <form>
+         <h1>RAFFLE MANIA</h1>
+         <p>Welcome To Raffle Mania. One Place To Shop Everything</p>
+         <div className='inputcon'>
+        
+            <h5>Username</h5>
+            <div className='speinput'>
+            <div>
+            <input className='A31' type='text'></input>
+            </div>
+            <div>
+            <i class="fa-solid fa-user"></i>
+            </div>
+        </div>
+        </div> <br/>
+         <div className='inputcon2'>
+        
+            <h5>Password</h5>
+            <div className='speinput2'>
+            <div>
+            <input className='A32' type='password'></input>
+            </div>
+            <div>
+            <i class="fa-solid fa-user"></i>
+            </div>
+        </div>
+        </div>
+     </form>
+     </div>
     </div>
 }
 export default Getin
